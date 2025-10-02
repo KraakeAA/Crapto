428ed45ccd3cdbb2b2f07321c3b5ae0e37e71986
KraakeAA <136975329+KraakeAA@users.noreply.github.com>
2025-10-03 00:45:44 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'crapto-brown': '#8B4513',
+        'crapto-light-brown': '#D2B48C',
+        'crapto-dark-brown': '#5C4033',
+      },
+    },
+  },
+  plugins: [],
+};
