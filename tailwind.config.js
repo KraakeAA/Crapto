34e59555b8b4ebd3cf39e857ba454666871255cd
KraakeAA <136975329+KraakeAA@users.noreply.github.com>
2025-10-03 13:07:33 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,1 @@
-module.exports = {
-  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
-  theme: {
-    extend: {
-      colors: {
-        'crapto-brown': '#7B3F00',
-        'crapto-light-brown': '#DAA06D',
-        'crapto-dark-brown': '#4A2C0A',
-      },
-      animation: {
-        'spin-slow': 'spin 2s linear infinite',
-      },
-    },
-  },
-  plugins: [],
-};
+
